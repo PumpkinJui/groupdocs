69f7b61d7de158f1329a04264604565873a91992
PumpkinJui <ZXN070320@163.com>
2025-08-22 11:49:58 +0800

    Update to Docusaurus 3.8

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
     favicon: 'img/yzbwdlt.webp',
 
     // Set the production url of your site here
-    url: 'https://github.com',
+    url: 'https://docs.nekoawa.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
