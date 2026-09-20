cb28ff9d4c341a2ea6028b8952dfc95628a3561a
PumpkinJui <ZXN070320@163.com>
2026-09-20 23:27:40 +0800

    [依赖] 初始化 ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        ...pluginReact.configs.flat.recommended,
+        settings: { react: { version: "detect" } },
+    },
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.css"],
+        plugins: { css },
+        language: "css/css",
+        extends: ["css/recommended"],
+    },
+]);
