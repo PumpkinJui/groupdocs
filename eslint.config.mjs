82d9e3ff4d0ae407d0bd61148943651c44bd46ea
PumpkinJui <ZXN070320@163.com>
2026-09-21 13:05:03 +0800

    [依赖] 添加 eslint-plugin-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import reactHooks from 'eslint-plugin-react-hooks'
 import json from "@eslint/json";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
@@ -16,6 +17,7 @@ export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,jsx}"],
         ...pluginReact.configs.flat.recommended,
+        ...reactHooks.configs.flat.recommended,
         settings: { react: { version: "detect" } },
     },
     {
